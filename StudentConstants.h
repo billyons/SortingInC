59fcc6861d8ef3475dbfd053a8f5d1090b0d582b
billyons <billyons@gmail.com>
2017-02-13 00:42:24 -0600

    Create StudentConstants.h

---
+++ StudentConstants.h
@@ -0,0 +1,9 @@
+/* This header for CSE1320 homework 2 was written by William Lyons on 10/5/2015 */
+
+#ifndef STUDENT_CONSTANTS_H
+#define STUDENT_CONSTANTS_H
+
+#define NUM_STUDENTS 7
+#define NUM_ATTRIBUTES 2
+
+#endif
